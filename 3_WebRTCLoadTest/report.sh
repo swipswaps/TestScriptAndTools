35f76f841595bfa4a33e897222abce03f2e5523a
burak-58 <burak@antmedia.io>
2018-11-27 17:24:44 +0300

    adapt to one instance setup

--- 3_WebRTCLoadTest/report.sh
+++ 3_WebRTCLoadTest/report.sh
@@ -1,26 +1,17 @@
 #plot the graphs
 gnuplot -persist <<-EOFMarker
 set datafile separator ','
-set term png size 1000,1000
+set term png size 500,1000
 set output 'measures.png'
 set xlabel 'clients'
-set ylabel 'cpu usage %'
 unset key
-set multiplot layout 3,2 columnsfirst
-set title "node-1"	
-plot 'measures.csv' using 1:3 with linespoints
-set title "node-2"
-plot 'measures.csv' using 1:7 with linespoints
-set title "node-3"
-plot 'measures.csv' using 1:11 with linespoints
-set xlabel 'clients'
-set ylabel 'ram usage MB'
-set title "node-1"	
-plot 'measures.csv' using 1:4 with linespoints
-set title "node-2"
-plot 'measures.csv' using 1:8 with linespoints
-set title "node-3"
-plot 'measures.csv' using 1:12 with linespoints
+set multiplot layout 2,1 columnsfirst
+set title "cpu"
+set ylabel 'cpu usage %'	
+plot 'measures.csv' using 1:2 with linespoints
+set title "memory"
+set ylabel 'ram usage MB'	
+plot 'measures.csv' using 1:(\$3/1024/1024) with linespoints
 unset multiplot
 EOFMarker
 
